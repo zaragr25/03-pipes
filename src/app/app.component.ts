fa9494f76cf2a3b13bdf117641e9a9198758a2af
Ricardo Zaragoza <zara.gr2591@gmail.com>
2019-11-27 14:36:27 -0600

    Final pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre = "Ricardo";
+  nombre2 = "ricArdo zaraGOza gaRcía";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  a: number = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre:"Logan",
+    clave:"Wolverine",
+    edad: 500, 
+    direccion:{
+      calle:"Primera",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise( (resolve, reject) => {
+
+    setTimeout( () => resolve('Llegó la data!'), 3500);
+  } );
+
+  fecha = new Date();
+
+  video = "69vtPrHgU9U";
+
+  activar:boolean = true;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
